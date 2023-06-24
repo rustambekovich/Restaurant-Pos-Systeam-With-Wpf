991acddf89fe705348c360023734f1df51ce1720
Muhammadqodir Yuldashev <muhammadqodir5050@gmail.com>
2023-06-25 02:00:37 +0500

    Products

--- Restaurant-Pos-Systeam-With-Wpf/Components/Items/ItemsUserControl.xaml.cs
+++ Restaurant-Pos-Systeam-With-Wpf/Components/Items/ItemsUserControl.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Restaurant_Pos_Systeam_With_Wpf.Domans.Entities;
+using Restaurant_Pos_Systeam_With_Wpf.Interfaces.Productes;
+using Restaurant_Pos_Systeam_With_Wpf.Repositories.Productes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,19 +23,29 @@ namespace Restaurant_Pos_Systeam_With_Wpf.Components.Items
     /// </summary>
     public partial class ItemsUserControl : UserControl
     {
+        private readonly IProductRepository _productRepository;
+        public long id { get; set; }
         public ItemsUserControl()
         {
             InitializeComponent();
+            this._productRepository = new ProductRepository();
         }
-
+        
         private void UserControl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
 
         }
-
         public void SetData(Products item)
         {
             itemImage.ImageSource =new BitmapImage(new Uri(item.ImagePath, UriKind.Relative));
+            List<Products> products = new List<Products>();
+            foreach (var product in _productRepository.GetAllAsync())
+            {
+
+            }
+            /*lbItemName.Content = .;
+            lbitemPrise.Content = .Price.ToString();*/
+
         }
     }
 }
